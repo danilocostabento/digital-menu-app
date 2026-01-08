d2203f5fd595631365fe32a44c059e83b0a96a24
Danilo Costa <91625647+danilocostabento@users.noreply.github.com>
2026-01-08 17:38:49 +0000

    🔥 Deleting unusable archives

--- src/routes/AppRoutes.tsx
+++
@@ -1,5 +0,0 @@
-/public/menu           → cardápio público
-/admin/login           → login
-/admin/dashboard       → painel
-/admin/menu            → CRUD itens
-/admin/users           → admins (MASTER)
